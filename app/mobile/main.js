30431363a35af4659be5d2bb96caa6316df4a091
wheezy <creativejason@gmail.com>
2014-10-29 18:12:18 -0700

    Adding initial files

---
+++ app/mobile/main.js
@@ -0,0 +1,106 @@
+$(function () {
+
+    "use strict";
+
+    var username = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)/i)[0] + ' User';
+    var ms = window.multiscreen;
+    var app;
+
+    var ui = {
+        castButton          : $('#castButton'),
+        castSettings         : $('#castSettings'),
+        castWindowTitle     : $('#castSettings .title'),
+        castWindowDeviceList: $('#castSettings .devices'),
+        castButtonDisconnect: $('#castSettings button.disconnect'),
+        castButtonRescan    : $('#castSettings button.search'),
+        phrase              : $('#txtName'),
+        sendButton          : $('#btnSend')
+    };
+
+    ms.on('discover', function(services){
+
+        ui.castWindowDeviceList.empty();
+
+        if(services.length > 0){
+            $(services).each(function(index, service){
+                $('<li>').text(service.device.name).data('service',service).appendTo(ui.castWindowDeviceList);
+            });
+            $('body').removeClass().addClass('disconnected');
+            ui.castWindowTitle.text('Connect To A Device');
+        }else{
+            $('<li>').text('No devices found').appendTo(ui.castWindowDeviceList);
+        }
+    });
+
+
+    var setService = function(service){
+
+        app = service.application({
+            id: window.location.href.replace('/mobile','/tv')
+        });
+
+        app.connect({name: username}, function (err) {
+            if(err) return console.error(err);
+        });
+
+        app.on('connect', function(){
+            $('body').removeClass().addClass('connected');
+            ui.castWindowTitle.text(service.device.name);
+        });
+
+        app.on('disconnect', function(){
+            $('body').removeClass().addClass('disconnected');
+            ui.castWindowTitle.text('Connect to a device');
+            app.removeAllListeners();
+        });
+
+    };
+
+    var init = function(){
+
+        ui.castButton.on('click', function(){
+            ui.castSettings.fadeToggle(200, 'swing');
+        });
+
+        ui.castSettings.on('click', function(evt){
+            evt.stopPropagation();
+            ui.castSettings.fadeOut(200, 'swing');
+        });
+
+        ui.castWindowDeviceList.on('click','li', function(evt){
+            evt.stopPropagation();
+            var service = $(this).data('service');
+            if(service){
+                setService(service);
+                ui.castSettings.hide();
+            }
+        });
+
+        ui.castButtonDisconnect.on('click', function(){
+            if(app) app.disconnect();
+            ui.castSettings.fadeToggle(200, 'swing');
+        });
+
+        ui.castButtonRescan.on('click', function(){
+            ms.search();
+        });
+
+        ui.sendButton.on('click', function(){
+            var msg = ui.phrase.val();
+            if(!msg) msg = 'You say it best when you say nothing at all!';
+            app.publish('say', msg);
+        });
+
+        ui.phrase.on('keyup', function(evt){
+            if (evt.keyCode === 13) {
+                ui.sendButton.trigger('click');
+            }
+        });
+
+        ms.search();
+    };
+
+    init();
+
+
+});
---
+++ app/tv/main.js
@@ -0,0 +1,43 @@
+$(function(){
+
+    "use strict";
+
+    var ms = window.multiscreen;
+    var app;
+
+    var ui = {
+        phrase : $('blockquote'),
+        author : $('sub')
+    };
+
+    ms.local(function(err, service){
+
+        app = service.application({
+            id: window.location.href,
+            stopOnDisconnect : false,
+            startOnConnect : false
+        });
+
+        app.connect({name: 'TV'}, function (err) {
+            if(err) return console.error(err);
+        });
+
+        app.on('say', function(msg, from){
+            ui.phrase.text(msg);
+            ui.author.text('~ ' + (from.attributes.name || 'Unknown'));
+        });
+
+        app.on('clientConnect', function(client){
+            ui.phrase.text('Hello '+client.attributes.name);
+            ui.author.text('~ Your TV');
+        });
+
+        app.on('clientDisconnect', function(client){
+            ui.phrase.text('Goodbye '+client.attributes.name);
+            ui.author.text('~ Your TV');
+        });
+
+    });
+
+});
+
